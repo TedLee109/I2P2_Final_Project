b1965ad8e7cb1b4fe6f9f98e39d296f2f2bb0674
TedLee109 <ted1050217@gmail.com>
2023-06-15 13:54:20 +0800

    MinMax deeper ver2

--- src/player/MinMax.cpp
+++ src/player/MinMax.cpp
@@ -41,7 +41,7 @@ void read_board(std::ifstream& fin) {
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
   while(true) {
-    auto move = MinMax::get_move(root, 2);
+    auto move = MinMax::get_move(root, 5);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
